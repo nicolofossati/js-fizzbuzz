4302a0226e1b05f163aa987f0f836996f17028f3
nicolo fossati <nicofossa01@gmail.com>
2023-02-24 16:04:47 +0100

    programma scritto completamente

--- js/main.js
+++ js/main.js
@@ -6,4 +6,18 @@ for(let i=1; i<=100; i++){
     boxDom.classList.add('default_box_color');
     boxDom.append(i); 
     containerDom.append(boxDom);
+
+    if((i%3 == 0) && (i%5 == 0)){
+        boxDom.classList.remove('default_box_color');
+        boxDom.classList.add('fizzbuzz_box_color');
+        boxDom.innerHTML = "fizzbuzz";
+    } else if(i%3 == 0){ // se i è multiplo di 3
+        boxDom.classList.remove('default_box_color');
+        boxDom.classList.add('fizz_box_color');
+        boxDom.innerHTML = "fizz";
+    } else if(i%5 == 0){ // se i è multiplo di 5
+        boxDom.classList.remove('default_box_color');
+        boxDom.classList.add('buzz_box_color');
+        boxDom.innerHTML = "buzz";
+    }
 }
